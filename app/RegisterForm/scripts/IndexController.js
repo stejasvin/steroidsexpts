a7a4dd1c09770d372dee219b2b6a091a1df35571
stejasvin <stejasvin@gmail.com>
2015-03-06 12:46:22 +0530

    File created in sdcard and data written

--- app/RegisterForm/scripts/IndexController.js
+++ app/RegisterForm/scripts/IndexController.js
@@ -8,123 +8,152 @@ steroids.logger.log("Inside registerForm indexController");
     
     steroids.logger.log("Inside controller");
 
-    $scope.takePic = function(){
-		navigator.camera.getPicture($scope.onSuccess, onFail, { quality: 50,
-		destinationType: Camera.DestinationType.FILE_URI });
-		steroids.logger.log("Inside takePic");
-	}
+// Wait for Cordova to load
+    //
+    document.addEventListener("deviceready", function() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, $scope.fail);
+        steroids.logger.log("Inside dr");
+    }, false);
 
-	$scope.onSuccess = function(imageURI) {
-    	// var image = document.getElementById('myImage');
-    	// image.src = imageURI;
-    	alert('success' + imageURI);
-    	steroids.logger.log("Inside success ");
-    	steroids.logger.log(imageURI);
-	}
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, $scope.fail);
+        //steroids.logger.log("Inside dr");
+    }
 
-	function onFail(message) {
-    	alert('Failed because: ' + message);
-    	steroids.logger.log("Inside success "+imageURI);
-	}
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("readme5.txt", {create: true}, 
+        	function(fileEntry) {
+		        fileEntry.createWriter(function(writer) {
+					//alert("Inside write")
+		        	// writer.onwriteend = function(evt) {
+		         //    //console.log("contents of file now 'some sample text'");
+			        //     writer.truncate(11);  
+			        //     writer.onwriteend = function(evt) {
+			        //         //console.log("contents of file now 'some sample'");
+			        //         writer.seek(4);
+			        //         writer.write(" different text");
+			        //         writer.onwriteend = function(evt){
+			        //             //console.log("contents of file now 'some different text'");
+			        //         }
+			        //     };
+			        // };
+			        writer.onwriteend = function(evt) {
+	                    alert("write success");
+	                };
+		        	writer.write("some sample text");
+		    	
+		    	}
+	    	, $scope.fail);
+	        //alert("Inside fe");
+	    }
+	, $scope.fail);
+        
+        //alert("Inside fs");
+    }
+
+    // function gotFileEntry(fileEntry) {
+    //     //alert(''+fileEntry.createWriter(gotFileWriter, fail));
+    //     alert("Inside fe");
+    // }
+
+    $scope.gotFileWriter = function(writer) {
+    	alert("Inside write");
+        writer.onwriteend = function(evt) {
+            console.log("contents of file now 'some sample text'");
+            writer.truncate(11);  
+            writer.onwriteend = function(evt) {
+                console.log("contents of file now 'some sample'");
+                writer.seek(4);
+                writer.write(" different text");
+                writer.onwriteend = function(evt){
+                    console.log("contents of file now 'some different text'");
+                }
+            };
+        };
+        writer.write("some sample text");
+    }
+
+    $scope.fail = function(error) {
+        alert(error.code);
+    }
+
+    // // function gotFileEntry(fileEntry) {
+    // //     fileEntry.createWriter(gotFileWriter, fail);
+    // //     alert("fe");
+    // // }
+
+    // // function gotFileWriter(writer) {
+    // // 	alert("writer");
+    // //     writer.onwriteend = function(evt) {
+    // //         console.log("contents of file now 'some sample text'");
+    // //         writer.truncate(11);
+    // //         writer.onwriteend = function(evt) {
+    // //             console.log("contents of file now 'some sample'");
+    // //             writer.seek(4);
+    // //             writer.write(" different text");
+    // //             writer.onwriteend = function(evt){
+    // //                 console.log("contents of file now 'some different text'");
+    // //             }
+    // //         };
+    // //     };
+    // //     writer.write("some sample text");
+    // // }
+
+    // function fail(error) {
+    //     alert("myerror: "+error.code);
+    // }
+
+    // document.addEventListener("deviceready", onDeviceReady, false);
+
+    // // device APIs are available
+    // //
+    // function onDeviceReady() {
+    //     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
+    //     window.resolveLocalFileSystemURL("file:///example.txt", onResolveSuccess, fail);
+    //     //steroids.logger.log("1");
+    //     alert('deviceready');
+    // }
+
+    // function onFileSystemSuccess(fileSystem) {
+    //     alert(fileSystem.name);
+    //     //steroids.logger.log("2");
+    //     //alert(deviceready);
+    // }
+
+    // function onResolveSuccess(fileEntry) {
+    //     alert(fileEntry.name);
+    //     //steroids.logger.log("3");
+    // }
+
+    // function fail(evt) {
+    //     alert(evt.target.error.code);
+    //     //steroids.logger.log("4");
+    // }
+
+ //    $scope.takePic = function(){
+	// 	navigator.camera.getPicture($scope.onSuccess, onFail, { quality: 50,
+	// 	destinationType: Camera.DestinationType.FILE_URI });
+	// 	steroids.logger.log("Inside takePic");
+	// }
+
+	// $scope.onSuccess = function(imageURI) {
+ //    	// var image = document.getElementById('myImage');
+ //    	// image.src = imageURI;
+ //    	steroids.logger.log("Inside success ");
+ //    	steroids.logger.log(imageURI);
+ //    	alert('success' + imageURI);
+    	
+	// }
+
+	// function onFail(message) {
+ //    	alert('Failed because: ' + message);
+ //    	steroids.logger.log("Inside success "+imageURI);
+	// }
 
 });
 
 
- //    var downloadedImage = new steroids.File("image.png");
-	// downloadedImage.resizeImage({
-	//   format: {
-	//     type: "jpg",
-	//     compression: 80
-	//   },
-	//   constraint: {
-	//     dimension: "width",
-	//     length: "150"
-	//   }
-	// }, {
-	//   onSuccess: function() {
-	//     alert("resized successfully");
-	//   },
-	//   onFailure: function() {
-	//     alert("resize failed");
-	//   }
-	// });
-
- //    function onInitFs(fs) {
- //  		steroids.logger.log('Opened file system: ' + fs.name);
-	// }
-
-	// window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onInitFs, errorHandler);
-
-	// function errorHandler(e) {
-	//   var msg = '';
-
-	//   switch (e.code) {
-	//     case FileError.QUOTA_EXCEEDED_ERR:
-	//       msg = 'QUOTA_EXCEEDED_ERR';
-	//       break;
-	//     case FileError.NOT_FOUND_ERR:
-	//       msg = 'NOT_FOUND_ERR';
-	//       break;
-	//     case FileError.SECURITY_ERR:
-	//       msg = 'SECURITY_ERR';
-	//       break;
-	//     case FileError.INVALID_MODIFICATION_ERR:
-	//       msg = 'INVALID_MODIFICATION_ERR';
-	//       break;
-	//     case FileError.INVALID_STATE_ERR:
-	//       msg = 'INVALID_STATE_ERR';
-	//       break;
-	//     default:
-	//       msg = 'Unknown Error';
-	//       break;
-	//   };
-
-	//   console.log('Error: ' + msg);
-	// }
-
- //    $scope.Apple = function(type) {
- //    this.type = type;
- //    this.color = "red";    
- //    this.getFullName = function(){
- //    	return this.type + "!" + this.color;
- //    }
-	// }
-
- //    var apple1 = new $scope.Apple('gen');
- //    $scope.name1 = apple1.getFullName();
- //    steroids.logger.log(apple1.type);
- //    steroids.logger.log(apple1.getFullName());
-
- //    onInitFs = function(fs) {
- //      steroids.logger.log("Inside onInitFs");
-
-	//   fs.root.getFile(cordova.file.externalDataDirectory+'/logsteroids.txt', {create: true}, function(fileEntry) {
-
-	//     // Create a FileWriter object for our FileEntry (log.txt).
-	//     fileEntry.createWriter(function(fileWriter) {
-
-	//       fileWriter.onwriteend = function(e) {
-	//         console.log('Write completed.');
-	//       };
-
-	//       fileWriter.onerror = function(e) {
-	//         console.log('Write failed: ' + e.toString());
-	//       };
-
-	//       // Create a new Blob and write it to log.txt.
-	//       var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
-
-	//       fileWriter.write(blob);
-
-	//     }, errorHandler);
-
-	//   }, errorHandler);
-
-	// }
-
-	// $scope.writeData = function(){
-	// 	steroids.logger.log("Inside writeData");
-	// 	window.requestFileSystem(window.PERSISTENT, 0, onInitFs, errorHandler);
-	// }
 
 
